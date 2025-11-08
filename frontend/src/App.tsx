529d5082dbe069d63e16e9d25871f3cf8d997ec8
John <j.naeder324@gmail.com>
2025-11-08 09:50:52 -0500

    Init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,51 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [userName, setUserName] = useState<string>("");
+  const [userMessage, setUserMessage] = useState<string>("");
+
+  const onButtonClick = () => {
+    console.log(userName, userMessage);
+  };
+
+  return (
+    <>
+      <div>
+        <h1 className="text-5xl font-bold">Small Scale Test Project</h1>
+        <p>
+          This is my test project for building frontend and backend website with
+          docker containers
+        </p>
+        <p>Please write your name in the box to record that you were here.</p>
+        <div className="w-1/2 mx-auto p-4">
+          <div className="flex justify-center mt-5 items-center gap-2">
+            <div>Name:</div>
+            <input
+              className="bg-white text-black rounded-2xl p-2 w-full"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center mt-5 items-center gap-2">
+            <div>Message:</div>
+            <textarea
+              className="bg-white text-black rounded-2xl p-2 w-full"
+              rows={3}
+              onChange={(e) => setUserMessage(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center mt-5 items-center gap-2">
+            <button
+              className="bg-blue-500 px-5 py-2 rounded-2xl text-2xl"
+              onClick={onButtonClick}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
