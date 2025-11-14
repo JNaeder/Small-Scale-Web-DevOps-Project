95d5ab5876adb90523958ac84d81515dc82840c0
John <j.naeder324@gmail.com>
2025-11-13 21:12:46 -0500

    Added more firewall stuff to terraform, created Docker compose and setup files

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,6 +33,47 @@ resource "digitalocean_droplet" "main" {
     ssh_keys = [digitalocean_ssh_key.ssh-key.id]
 }
 
+resource "digitalocean_firewall" "ssh-http" {
+  name = "ssh-and-http"
+  droplet_ids = [digitalocean_droplet.main.id]
+  
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "22"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "80"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol = "icmp"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol = "tcp"
+    port_range = "1-65535"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol = "udp"
+    port_range = "1-65535"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol = "icmp"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+}
+
+
 resource "digitalocean_project" "small-scale-devops-project" {
     name = "Small Scale DevOps Project"
     environment = "Production"
